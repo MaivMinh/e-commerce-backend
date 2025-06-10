cce79d1515b1fe7b72223206dc9fd070cf2a1321
MaivMinh <maivanminh.se@gmail.com>
2025-06-10 17:36:06 +0700

    Update product service

--- app/product-service/src/main/java/com/minh/product_service/repository/ProductVariantRepository.java
+++ app/product-service/src/main/java/com/minh/product_service/repository/ProductVariantRepository.java
@@ -16,13 +16,13 @@ public interface ProductVariantRepository extends JpaRepository<ProductVariant, 
 
   @Query(value = "" +
           "select pvs.id as productVariantId, pvs.size as productVariantSize, pvs.color_name as productVariantColorName, pvs.color_hex as productVariantColorHex, pvs.price as productVariantPrice, pvs.original_price as productVariantOriginalPrice, P.name as productName, P.cover as productCover, P.slug as productSlug " +
-          "from product_variants pvs join Products P on pvs.product_id = P.id " +
+          "from product_variants pvs join products P on pvs.product_id = P.id " +
           "where pvs.id = :productVariantId ",
           nativeQuery = true)
   Optional<ProductCartDTO> findProductCartDTOByProductVariantId(String productVariantId);
 
   @Query(value = "" +
-          "select P.name as productName, P.slug as productSlug, P.cover as productCover, pvs.size as productVariantSize, pvs.color_name as productVariantColorName, pvs.color_hex as productVariantColorHex, pvs.price as productVariantPrice, pvs.original_price as productVariantOriginalPrice from product_variants pvs join Products P on pvs.product_id = P.id " +
+          "select P.name as productName, P.slug as productSlug, P.cover as productCover, pvs.size as productVariantSize, pvs.color_name as productVariantColorName, pvs.color_hex as productVariantColorHex, pvs.price as productVariantPrice, pvs.original_price as productVariantOriginalPrice from product_variants pvs join products P on pvs.product_id = P.id " +
           "where pvs.id = :productVariantId", nativeQuery = true)
   ProductVariantMessageDTO findProductVariantById(String productVariantId);
 
